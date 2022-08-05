52e8c209c242b0eeed15ee66fa610cfa3da653f7
Martynas R <m.rafanavicius@gmail.com>
2022-08-05 21:19:06 +0300

    Add project files.

---
+++ JWT/Dtos/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace JWT.Dtos
+{
+    public class UserDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
